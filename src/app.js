796b3e96cd8718b9f10400c5482ce88a218b5cd2
Unibik21 <utkarshcha21@gmail.com>
2026-02-07 16:40:37 +0530

    User Auth Middleware Added

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const connectDB = require('./config/database');
 const bcrypt = require('bcrypt');
 const {validateSignUpData} = require('./utils/validation');
 const cookieParser = require('cookie-parser');
-const jwt =require('jsonwebtoken')
+const jwt =require('jsonwebtoken');
+const userAuth = require('./middlewares/auth')
 const app = express();
 
 app.use(express.json());
@@ -43,7 +44,9 @@ app.post("/login",async (req,res)=>{
             throw new Error("Password Not Valid");
         }
         else{
-            const token = jwt.sign({_id:user._id},"CodeCrush@123");
+            const token = jwt.sign({_id:user._id},"CodeCrush@123",{
+                expiresIn:"1d",
+            });
             res.cookie("token",token);
             res.send("LOGIN SUCCESSFULL");
         }
@@ -54,72 +57,16 @@ app.post("/login",async (req,res)=>{
     
 });
 
-app.get("/profile",async(req,res)=>{
-
+app.get("/profile",userAuth,async(req,res)=>{
+    const {user} = req;
     try{
-        const cookie = req.cookies;
-        const {token} = cookie;
-        if(!token){
-            throw new Error("PLEASE LOGIN");
-        }
-        const decoded = jwt.verify(token,"CodeCrush@123");
-        const {_id} = decoded;
-        const user = await User.findById(_id);
-        if(!user){
-            throw new Error("NO USER FOUND");
-        }
         res.send(user);
     }
     catch(err){
         res.status(400).send("ERROR : "+ err.message);
     }
-});
-
-app.get('/feed',async(req,res)=>{
-    try{
-        const users = await User.find({});
-        res.send(users);    
-    }
-    catch(err){
-        res.status(400).send("something went wrong");
-    }
-});
-
-app.delete('/user',async(req,res)=>{
-    const userId = req.body.userId;
-    try{
-        await User.findByIdAndDelete(userId);
-        res.send("User Deleted Successfully!");
-    }   
-    catch(err){
-        res.status(400).send("Something went wrong!"); 
-    }
-});
-
-app.patch('/user/:userId',async(req,res)=>{
-    const userId = req.params?.userId;
-    const data = req.body;
-
-    try{
-        const ALLOWED_UPDATES = ['photoURL','About','skills','age','gender'];
-        const isUpdateAllowed = Object.keys(data).every((k)=> ALLOWED_UPDATES.includes(k));
-
-        if(!isUpdateAllowed){
-            throw new Error("UPDATE NOT ALLOWED");
-        }
-
-        if(data?.skills.length>10){
-            throw new Error("MAXIMUM 10 SKILLS ALLOWED");
-        }
-
-        await User.findByIdAndUpdate(userId,data,{runValidators:true});
-        res.send("User Updated Successfully");
-    }
-    catch(err){
-        res.status(400).send("User Not Updated");
-    }
-})
-
+}); 
+ 
 connectDB()
 .then(() => {
     console.log("Server Connected Successfully!");
---
+++ src/middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const userAuth = async(req,res,next) =>{
+    const token = req.cookies.token;
+    if(!token){
+        throw new Error("PLEASE LOGIN");
+    }
+    const decodedObj = jwt.verify(token,"CodeCrush@123");
+    const _id = decodedObj._id;
+    const user = await User.findById(_id);
+    if(!user){
+        throw new Error("USER NOT FOUND");
+    }
+    req.user = user;
+    next();
+}
+
+module.exports = userAuth;
