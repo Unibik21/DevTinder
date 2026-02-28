5eb324b9dc905e937f68e3795704cf1330483d36
Unibik21 <utkarshcha21@gmail.com>
2026-02-28 13:13:43 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://16.170.215.1:3000",
     credentials:true,
 }));
 app.use(express.json());
